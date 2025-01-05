4c7d7d08c85a4d34b7e27eb90a346224100d4ad9
veeresh <veereshattuluri123@gmail.com>
2025-01-05 12:40:47 +0530

    addded

--- 13-logs.sh
+++ 13-logs.sh
@@ -5,7 +5,7 @@ Y="\e[33m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date  date +%Y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.LOG"
 
 
