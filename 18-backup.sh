ef8d7b735285ad1ccb0f2a84d5e1a234bcfa8c15
veeresh <veereshattuluri123@gmail.com>
2025-01-06 22:15:20 +0530

    addded

--- 18-backup.sh
+++ 18-backup.sh
@@ -54,4 +54,9 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
- echo "Files are: $FILES"
+if [ -n "$FILES" ] # true if there are files to zip
+then
+    echo "Files are: $FILES"
+else   
+    echo "No files found older than $DAYS"
+fi
