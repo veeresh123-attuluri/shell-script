5f7a55fca8524fca2a5d85a0a9a5ff4dd88d1991
veeresh <veereshattuluri123@gmail.com>
2025-01-06 21:16:01 +0530

    addded

---
+++ 18-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+USAGE(){
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+}
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
